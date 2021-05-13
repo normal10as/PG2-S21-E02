32c22ab722a76373451c13c67431548b1b5a6990
Fabio Dos Santos <fabio2st@yahoo.com>
2021-05-13 20:32:10 -0300

    Procedures

---
+++ Procedimientos/ProcedimientosCs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProcedimientosCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumar(10, 30);
+            Console.WriteLine();
+        }
+
+        static void Sumar(int valor1, int valor2)
+        {
+            Console.WriteLine(valor1 + valor2);
+        }
+
+        static string Concatenar(string valor1, string valor2) 
+        {
+            return valor1 + valor2;
+        }
+    }
+}
--- Procedimientos/ProcedimientosVb/Program.vb
+++ Procedimientos/ProcedimientosVb/Program.vb
@@ -1,9 +1,28 @@
 Imports System
+Imports System.Drawing
 
 Module Program
+    Friend vari As String
     Sub Main(args As String())
         Sumar(10, 30)
         Console.WriteLine(Concatenar("Hola ", "mundo"))
+        Dim a As Int16 = 10
+        Dim b As Int16 = a
+        a = 20
+        Console.WriteLine("{0}, {1}", a, b)
+
+        Dim c, d As String
+        c = "hola"
+        d = c
+        c = "mundo"
+        Console.WriteLine("{0}, {1}", c, d)
+
+        Dim e, f As New DateTime()
+        e = DateTime.Now
+        f = e
+        f = f.AddDays(100)
+        Console.WriteLine("{0}, {1}", e, f)
+
     End Sub
 
     Sub Sumar(valor1 As Integer, valor2 As Integer)
